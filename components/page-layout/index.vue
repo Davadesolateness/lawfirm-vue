e2809793d8fbf1b8254397380fe89c4569cd8c24
dongzhibo <1216060227@qq.com>
2025-04-06 18:08:41 +0800

    修改自定义标签页面

---
+++ components/page-layout/index.vue
@@ -0,0 +1,25 @@
+<template>
+  <view class="page-container">
+    <view class="page-content">
+      <slot></slot>
+    </view>
+    <CustomTabBar></CustomTabBar>
+  </view>
+</template>
+
+<script setup>
+import CustomTabBar from '@/components/custom/tabbar'
+</script>
+
+<style>
+.page-container {
+  display: flex;
+  flex-direction: column;
+  min-height: 100vh;
+}
+
+.page-content {
+  flex: 1;
+  padding-bottom: 50px; /* 为底部tabBar预留空间 */
+}
+</style> 
